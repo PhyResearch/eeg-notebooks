679fe138093de0c837aae220d829950f60bfc3b4
Guillermo Bernal <gbernal@mit.edu>
2022-01-23 18:22:23 -0500

    crashed issue was resolved by upgrading psychopy to latest

--- tests/testing_galea_brainflow.py
+++ tests/testing_galea_brainflow.py
@@ -3,12 +3,12 @@ from eegnb import generate_save_fn
 from eegnb.devices.eeg import EEG
 from eegnb.experiments.visual_n170 import n170
 # define the name for the board you are using and call the EEG object
-# board_name = 'galea'
-board_name = 'cyton'
-# ip_address = '192.168.5.228'
+board_name = 'galea'
+#board_name = 'cyton'
+ip_address = '192.168.5.94'
 experiment = 'visual_n170'
-#eeg_device = EEG(device=board_name, ip_addr=ip_address)
-eeg_device = EEG(device=board_name)
+eeg_device = EEG(device=board_name, ip_addr=ip_address)
+#eeg_device = EEG(device=board_name)
 record_duration = 120
 session = '0o1'
 subject = 0o1 # a 'very British number'
